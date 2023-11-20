4274cbf95b7b814f82605a399d2f5fcd288497e0
vov4ik-v <83838066+vov4ik-v@users.noreply.github.com>
2023-11-21 01:54:48 +0200

    added lab5 file with source code and described the task in readme

---
+++ lab.py
@@ -0,0 +1,132 @@
+class Flower:
+    # Приватні атрибути класу для представлення характеристик квітки
+    __height = None
+    __size = None
+    __color = None
+    __price = None
+    __quantity = None
+    __delivery_rate = None
+
+    # Методи для отримання значень приватних атрибутів
+    def get_height(self):
+        return self.__height
+
+    def get_size(self):
+        return self.__size
+
+    def get_color(self):
+        return self.__color
+
+    def get_price(self):
+        return self.__price
+
+    def get_quantity(self):
+        return self.__quantity
+
+    def get_delivery_rate(self):
+        return self.__delivery_rate
+
+    # Методи для встановлення значень приватних атрибутів
+    def set_height(self, value):
+        self.__height = value
+
+    def set_size(self, value):
+        self.__size = value
+
+    def set_color(self, value):
+        self.__color = value
+
+    def set_price(self, value):
+        self.__price = value
+
+    def set_quantity(self, value):
+        self.__quantity = value
+
+    def set_delivery_rate(self, value):
+        self.__delivery_rate = value
+
+    # Конструктор класу, який ініціалізує атрибути квітки
+    def __init__(self, height, size, color, price, quantity, delivery_rate):
+        self.__height = height
+        self.__size = size
+        self.__color = color
+        self.__price = price
+        self.__quantity = quantity
+        self.__delivery_rate = delivery_rate
+
+    # Метод для представлення квітки у вигляді рядка
+    def __str__(self):
+        return f"Flower: | Height:{self.get_height()} Price:{self.get_price()} Quantity:{self.get_quantity()}|"
+
+
+class FlowerShop:
+    def __init__(self):
+        # Ініціалізуємо список для зберігання квітів у квітковому магазині
+        self.list_of_flowers_in_shop = []
+
+    # Метод для додавання квітки до магазину
+    def add_flower_to_shop(self, flower):
+        self.list_of_flowers_in_shop.append(flower)
+
+    # Метод для отримання списку найдорожчих квітів
+    def top_most_expensive_flowers(self):
+        sorted_list = sorted(self.list_of_flowers_in_shop, key=lambda flower: flower.get_price(), reverse=True)
+        return sorted_list
+
+    # Метод для видалення квітки з магазину
+    def delete_flower_to_shop(self, flower):
+        if flower in self.list_of_flowers_in_shop:
+            self.list_of_flowers_in_shop.remove(flower)
+
+    # Метод для представлення інформації про квітки в магазині у вигляді рядка
+    def __str__(self):
+        return "\n".join(str(flower) for flower in self.list_of_flowers_in_shop)
+
+
+class Bouquet:
+    def __init__(self):
+        self.flowers = []
+
+    # Метод для додавання квіток до букету з вказаною кількістю
+    def add_flower_to_bouquet(self, flower, quantity):
+        for _ in range(quantity):
+            self.flowers.append(flower)
+
+    # Метод для обчислення загальної ціни букету
+    def get_total_price(self):
+        return sum(flower.get_price() for flower in self.flowers)
+
+    # Метод для представлення інформації про букет у вигляді рядка
+    def __str__(self):
+        return f"Bouquet - Total Price: {self.get_total_price()}"
+
+
+if __name__ == "__main__":
+    # Створення об'єктів квіток з різними характеристиками
+    rose = Flower(20, "Medium", "Red", 5.99, 10, 1)
+    tulip = Flower(15, "Small", "Yellow", 3.99, 15, 0.5)
+    lily = Flower(25, "Large", "White", 6.99, 8, 2)
+
+    # Створення об'єкта квіткового магазину та додавання квіток до нього
+    flower_shop = FlowerShop()
+    flower_shop.add_flower_to_shop(rose)
+    flower_shop.add_flower_to_shop(tulip)
+    flower_shop.add_flower_to_shop(lily)
+
+    # Виведення інвентаря магазину у консоль
+    print("Flower Shop Inventory:")
+    print(flower_shop)
+
+    # Створення букету та додавання квіток до нього
+    bouquet = Bouquet()
+    bouquet.add_flower_to_bouquet(rose, 3)
+    bouquet.add_flower_to_bouquet(tulip, 5)
+
+    # Виведення деталей букету у консоль
+    print(f"Bouquet Details: {bouquet}")
+
+    # Знаходження та виведення найдорожчих квіток у магазині
+    top_expensive_flowers = flower_shop.top_most_expensive_flowers()
+    print("Top Most Expensive Flowers in the Shop:")
+    for i, flower in enumerate(top_expensive_flowers, start=1):
+        print(f"{i}. {flower}")
